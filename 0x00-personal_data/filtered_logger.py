d3a7de0a0867f4562f1b5d53925579ecd17313bb
Ken Wekesa <wanyamak884@gmail.com>
2025-08-04 15:22:43 +0300

    pycodestyle

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Regex-ing"""
 import re
+import typing
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: typing.List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """Returns log message with specified fields redacted."""
     pattern = f"({'|'.join(fields)})=.*?(?={separator}|$)"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
