a041f256e6c5e5581356b2ee06ebaf7f18d2d655
Ken Wekesa <wanyamak884@gmail.com>
2025-08-06 17:07:22 +0300

    strict_slashes was not set to False

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,7 +27,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized')
+@app_views.route('/unauthorized', strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     """
