5049e6987b87a87c4775f0e48dbe6fd631b534ff
Ken Wekesa <wanyamak884@gmail.com>
2025-08-09 18:48:17 +0300

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -29,13 +29,17 @@ if getenv("AUTH_TYPE"):
 def filter_request():
     """ filter each request"""
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
+    from api.v1.auth.session_auth import SessionAuth
     if auth is None:
         return
     request.current_user = auth.current_user(request)
     if not auth.require_auth(request.path, excluded_paths):  # type: ignore
         return
-    if auth.authorization_header(request) is None:
+    session_id = SessionAuth.user_id_by_session_id
+    print(session_id)
+    if auth.session_cookie(request) is \
+            None and auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
