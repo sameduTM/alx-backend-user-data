650d237e372503a9ca7e3981f0ee531672946c10
Ken Wekesa <wanyamak884@gmail.com>
2025-08-10 20:39:20 +0300

    Empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -21,6 +21,9 @@ if getenv("AUTH_TYPE"):
     if getenv("AUTH_TYPE") == "basic_auth":
         from api.v1.auth.basic_auth import BasicAuth
         auth = BasicAuth()
+    if getenv("AUTH_TYPE") == "session_auth":
+        from api.v1.auth.session_auth import SessionAuth
+        auth = SessionAuth()
 
 
 @app.before_request
