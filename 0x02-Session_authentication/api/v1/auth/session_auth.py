46dcef8fdba87d963a0763b8886a52709600026a
Ken Wekesa <wanyamak884@gmail.com>
2025-08-10 20:47:47 +0300

    trailing whitespace

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -17,4 +17,4 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         SessionAuth.user_id_by_session_id[session_id] = user_id
 
-        return session_id        
+        return session_id
