d64d97cf02aa05dce658ba5bfc319bb911d08236
Ken Wekesa <wanyamak884@gmail.com>
2025-08-13 12:48:09 +0300

    create User

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """User model"""
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -13,5 +13,5 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=False)
-    reset_token = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
