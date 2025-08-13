73d88beade7564f0f2845f73de3228fdc8d77412
Ken Wekesa <wanyamak884@gmail.com>
2025-08-13 12:52:46 +0300

    annotate function, echo=False

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -14,7 +14,7 @@ class DB:
 
     def __init__(self) -> None:
         """Initialize a new DB instance"""
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -27,11 +27,11 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """returns a user object"""
         user = User(email=email, hashed_password=hashed_password)
         session = self._session
         session.add(user)
-        session.commit()
+        session.flush()
 
         return user
