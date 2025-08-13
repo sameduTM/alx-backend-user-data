0534ee9763c34a3f6ac611c2f269e55316e05421
Ken Wekesa <wanyamak884@gmail.com>
2025-08-13 17:02:47 +0300

    pycodestyle validation

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -52,5 +52,7 @@ class DB:
     def update_user(self, user_id: int, **kwargs) -> None:
         """update user's attribute as passed and commit to db"""
         for key, value in kwargs.items():
-            self._session.query(User).filter(User.id == user_id).update({key: value})
+            self._session.query(
+                User).filter(
+                    User.id == user_id).update({key: value})  # type: ignore
         self._session.commit()
