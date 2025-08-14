6b00a705d1678c9ced478e309777a273278aaf1f
Ken Wekesa <wanyamak884@gmail.com>
2025-08-14 15:48:51 +0300

    User profile route created

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,6 +49,15 @@ def logout():
         return redirect('/', code=302)
     abort(403)
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """function to respond to the GET /profile route"""
+    session_id = str(request.cookies.get('session_id'))
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    abort(403)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
