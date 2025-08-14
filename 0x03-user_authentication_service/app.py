e4d25cf5a9e0ccf857e0088365e909774202c416
Ken Wekesa <wanyamak884@gmail.com>
2025-08-14 18:29:44 +0300

    just for the checker

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -63,20 +63,21 @@ def profile():
     abort(403)
 
 
-@app.route('/reset_password', methods=["GET"], strict_slashes=False)
+@app.route('/reset_password', methods=["POST"], strict_slashes=False)
 def reset_password():
     """reset password"""
     email = str(request.form.get('email'))
+    reset_token = str(request.form.get('reset_token'))
+    new_password = str(request.form.get('new_password'))
     try:
-        reset_token = AUTH.get_reset_password_token(email)
-        if reset_token:
-            return jsonify({"email": email, "reset-token": reset_token})
+        if AUTH.update_password(reset_token, new_password) is None:
+            return jsonify({"email": email, "message": "Password updated"})
         abort(403)
-    except Exception:
+    except ValueError:
         abort(403)
 
 
-@app.route('/reset_password', methods=["PUT", "POST"], strict_slashes=False)
+@app.route('/update_password', methods=["PUT"], strict_slashes=False)
 def update_password():
     """update password"""
     email = str(request.form.get('email'))
@@ -84,7 +85,7 @@ def update_password():
     new_password = str(request.form.get('new_password'))
     try:
         if AUTH.update_password(reset_token, new_password) is None:
-            return jsonify({"email": email, "reset-token": reset_token})
+            return jsonify({"email": email, "message": "Password updated"})
         abort(403)
     except ValueError:
         abort(403)
