d26427de8e8ec5e2cf3b3d2efc62e3be2051a71a
Ken Wekesa <wanyamak884@gmail.com>
2025-08-15 09:25:15 +0300

    user profile, remove redundant code

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -60,10 +60,9 @@ def logout():
 def profile():
     """function to respond to the GET /profile route"""
     session_id = str(request.cookies.get('session_id'))
-    if session_id:
-        user = AUTH.get_user_from_session_id(session_id)
-        if user:
-            return jsonify({"email": user.email}), 200
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
     abort(403)
 
 
